2b6435ce077afd0414223f75c83fa926e0496fb1
Michael Chan <michaelcychan@gmail.com>
2022-11-05 20:43:14 +0000

    add pull down menu to change grid size with provided options

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import PropTypes from 'prop-types';
 
 import './App.css';
@@ -7,15 +7,31 @@ import {GenerateNumberGrid} from './components/generateNumberGrid'
 
 function App() {
 
-  const arrayNum: Number[] = Array.from(Array(100).keys()).map(x => x+1);
+  const [gridSize, setGridSize] = useState(100);
+  const arrayNum: Number[] = Array.from(Array(gridSize).keys()).map(x => x+1);
+  const gridSizeOption: Number[] = [100, 200, 500, 10000];
+
+  
+
+  const changeGridSize = (e: ChangeEvent<HTMLSelectElement>) => {
+    const newValue: string = e.target.value
+    setGridSize(Number(newValue))
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        
-          Learn Maths Together!
-
+          一起學數學！
       </header>
+      <section id="grid-size-setter">
+        <label htmlFor="grid-size">要多少格：</label>
+        <select id="grid-size" name="grid-size" onChange={changeGridSize}>
+          {gridSizeOption.map(element => {
+            return <option key={element.toString()} value={element.toString()}>{element.toString()}</option>
+          })}
+
+        </select>
+      </section>
       <section>
         <GenerateNumberGrid arrayNum={arrayNum}/>
       </section>
