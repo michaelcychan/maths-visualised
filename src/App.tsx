c2bd5d5e5d69cf427a07e7d089288da4762cfce3
Michael Chan <michaelcychan@gmail.com>
2022-11-08 18:38:46 +0000

    add reset button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, {ChangeEvent, useState, MouseEvent} from 'react';
-import PropTypes from 'prop-types';
 
 import './App.css';
 
@@ -13,7 +12,8 @@ function App() {
 
   const [gridSize, setGridSize] = useState(100);
 
-  const [selectedNumbers, setSelectedNumbers] = useState(new Array<string>());
+  const emptySelectedNumbers = new Array<string>()
+  const [selectedNumbers, setSelectedNumbers] = useState(emptySelectedNumbers);
   
   const gridClick = (e: MouseEvent<HTMLElement>) => {
     const text = extractInnerTextFromEventTarget(e.target);
@@ -22,6 +22,10 @@ function App() {
     } else {
       setSelectedNumbers(prevState => {return prevState.filter(num => num !== text)});
     }
+  }
+
+  const resetSelection = () => {
+    setSelectedNumbers(emptySelectedNumbers);
   }
 
   const changeGridSize = (e: ChangeEvent<HTMLSelectElement>) => {
@@ -40,11 +44,13 @@ function App() {
         <section>請用滑鼠選取以下的數字。選取了的數字的倍數會變黑。<br />
         想想看，為甚麼有些數字怎樣也不能變黑？<br />
         <div>
-        已塗黑這些數字的2倍以上的倍數：
+        已塗黑這些數字的<em>2倍</em>以上的倍數：<br />
         {selectedNumbers.map(numText => {
           return <span>{numText} </span>;
-        })}
+        })} <br />
         </div>
+        <div><button onClick={resetSelection}>由頭開始</button></div>
+        
         </section>
       </div>
       <br></br>
