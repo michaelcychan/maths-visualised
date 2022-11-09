465b8fa0adda6f7121c8647a11a9790d1cb5a2be
Michael Chan <michaelcychan@gmail.com>
2022-11-09 14:59:50 +0000

    add buttons to homepage to switch to different pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,45 @@
-import React, {ChangeEvent, useState, MouseEvent} from 'react';
+import React, {useState, MouseEvent, Key} from 'react';
 
 import './App.css';
 
-import {GenerateNumberGrid} from './components/generateNumberGrid'
-import {GridSizeSelector} from './components/gridSizeSelector';
-import {extractInnerTextFromEventTarget} from './components/extractInnerTextFromEventTarget';
-import {sortThisArray} from './components/sortThisArray';
-import {ExplanationPrime} from './components/explanationPrime';
+import {PrimeNumberPage} from './components/primeNumberPage';
 
 function App() {
 
-  const [gridSize, setGridSize] = useState(100);
+  type pageType = "Prime" | "Factors" | "Multiples"
+  const initialPage: pageType = "Prime";
 
-  const emptySelectedNumbers = new Array<string>()
-  const [selectedNumbers, setSelectedNumbers] = useState(emptySelectedNumbers);
-  
-  const gridClick = (e: MouseEvent<HTMLElement>) => {
-    const text = extractInnerTextFromEventTarget(e.target);
-    if (selectedNumbers.indexOf(text) === -1) {
-      setSelectedNumbers(prevState => {return sortThisArray([...prevState, text])});
+  const [showPage, setShowPage] = useState<pageType>(initialPage)
+
+
+  const availablePages: String[] = ["質數", "因數", "倍數"]
+    
+  const buttonClicked = (e: MouseEvent<HTMLElement>) => {
+    const button = (e.target as HTMLElement).innerHTML;
+    console.log(button)
+    if (button === "質數") {
+      setShowPage("Prime")
+    } else if (button === "因數") {
+      setShowPage("Factors")
     } else {
-      setSelectedNumbers(prevState => {return prevState.filter(num => num !== text)});
+      setShowPage("Multiples")
     }
   }
-
-  const resetSelection = () => {
-    setSelectedNumbers(emptySelectedNumbers);
-  }
-
-  const changeGridSize = (e: ChangeEvent<HTMLSelectElement>) => {
-    const newValue: string = e.target.value
-    setGridSize(Number(newValue))
-  }
-
+  
   return (
     <div className="App">
       <header className="App-header">
           一起學數學！
       </header>
-      <GridSizeSelector changeGridSize={changeGridSize} />
-      <div>
-        <ExplanationPrime />
-        <section>請用滑鼠選取以下的數字。選取了的數字的倍數會變黑。<br />
-        想想看，為甚麼有些數字怎樣也不能變黑？<br />
-        <div>
-        已塗黑這些數字的<em>2倍</em>以上的倍數：<br />
-        {selectedNumbers.map(numText => {
-          return <span>{numText} </span>;
-        })} <br />
-        </div>
-        <div><button onClick={resetSelection}>由頭開始</button></div>
-        
-        </section>
-      </div>
-      <br></br>
-      <section>
-        <GenerateNumberGrid gridSize={gridSize} click={gridClick} selected={selectedNumbers}/>
-      </section>
+      {availablePages.map(page => {
+      return <button key={page as Key} onClick={buttonClicked}>{page}</button>
+    })}
+    {showPage === "Prime"
+      ? <PrimeNumberPage />
+      : showPage === "Factors"
+        ? <div>因數 - 未整好</div>
+        : <div>倍數 - 未整好</div>
+    }
       
     </div>
   );
---
+++ src/components/primeNumberPage.tsx
@@ -0,0 +1,59 @@
+import React, {ChangeEvent, useState, MouseEvent} from 'react';
+
+
+import '../App.css';
+
+import {GenerateNumberGrid} from './generateNumberGrid'
+import {GridSizeSelector} from './gridSizeSelector';
+import {extractInnerTextFromEventTarget} from './extractInnerTextFromEventTarget';
+import {sortThisArray} from './sortThisArray';
+import {ExplanationPrime} from './explanationPrime';
+
+export const PrimeNumberPage = () => {
+  const [gridSize, setGridSize] = useState(100);
+
+  const emptySelectedNumbers = new Array<string>()
+  const [selectedNumbers, setSelectedNumbers] = useState(emptySelectedNumbers);
+  
+  const gridClick = (e: MouseEvent<HTMLElement>) => {
+    const text = extractInnerTextFromEventTarget(e.target);
+    if (selectedNumbers.indexOf(text) === -1) {
+      setSelectedNumbers(prevState => {return sortThisArray([...prevState, text])});
+    } else {
+      setSelectedNumbers(prevState => {return prevState.filter(num => num !== text)});
+    }
+  }
+
+  const resetSelection = () => {
+    setSelectedNumbers(emptySelectedNumbers);
+  }
+
+  const changeGridSize = (e: ChangeEvent<HTMLSelectElement>) => {
+    const newValue: string = e.target.value
+    setGridSize(Number(newValue))
+  }
+
+  return (
+    <>
+      <GridSizeSelector changeGridSize={changeGridSize} />
+      <div>
+        <ExplanationPrime />
+        <section>請用滑鼠選取以下的數字。選取了的數字的倍數會變黑。<br />
+        想想看，為甚麼有些數字怎樣也不能變黑？<br />
+        <div>
+        已塗黑這些數字的<em>2倍</em>以上的倍數：<br />
+        {selectedNumbers.map(numText => {
+          return <span>{numText} </span>;
+        })} <br />
+        </div>
+        <div><button onClick={resetSelection}>由頭開始</button></div>
+        
+        </section>
+      </div>
+      <br></br>
+      <section>
+        <GenerateNumberGrid gridSize={gridSize} click={gridClick} selected={selectedNumbers}/>
+      </section>
+    </>
+  )
+}
