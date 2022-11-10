657542d9907d0f85c80ef6ae33a6e1a3829b1206
Michael Chan <michaelcychan@gmail.com>
2022-11-10 15:25:05 +0000

    add colours to frame class, add frame class to maths forumla

--- src/components/explanationFactor.tsx
+++ src/components/explanationFactor.tsx
@@ -5,14 +5,14 @@ export const ExplanationFactor = () => {
       <p>
         因數 (factor) 是指可以整除另一個整數的數字。<br />
         例如，12 的因數包括 1, 2, 3, 4, 6, 12 。大部分的因數都可以配對，例如：<br />
-        1 x 12; 2 x 6; 3 x 4 <br />
+        <span className="frame">1 x 12</span>; <span className="frame">2 x 6 </span>; <span className="frame">3 x 4 </span><br />
         有時候，因數要和自己配對，這出現在平方數 (Square numbers)上。例如： 16<br />
-        1 x 16; 2 x 8; 4 x 4 <br />
-        16 的因數是 1, 2, 4, 6, 16。由上述可見，4是不能和另一個因數配對，因為16是4的平方數。<br />
+        <span className="frame">1 x 16 </span>; <span className="frame">2 x 8 </span>; <span className="frame">4 x 4 </span><br />
+        16 的因數是 1, 2, 4, 6, 16。由上述可見，4 只能和自己配對，因為16是4的 <span className="frame">平方數</span>。<br />
         「平方」(Square)的意思就是 n x n，另一個寫法就是 n<sup>2</sup>。 <br />
         所以 4 的平方是16。倒過來說，16的平方根 (Square root) 是 4。<br />
         數學上，我們這樣表達： <br /> <br />
-        4<sup>2</sup> = 16 或  <span className="square-root">&radic;<span className="square-root-number">&nbsp;16&nbsp;</span> = 4 <br />
+        <span className="frame"> 4 <sup>2</sup> = 16</span> 或 <span className="frame"> 16 &divide; 4 = 4 </span> 或 <span className="frame"> <span className="square-root">&radic;<span className="square-root-number">&nbsp;16&nbsp;</span> = 4 </span><br />
 </span>
       </p>
       <p>
--- src/components/explanationPrime.tsx
+++ src/components/explanationPrime.tsx
@@ -4,8 +4,8 @@ export const ExplanationPrime = () => {
       <h2>倍數與質數</h2>
       <p>
         質數 (prime number) 的定義是只有兩個因數 (factor)。<br />
-        例如 7 是質數，因為它的因數只有 1 和 7 ﹝共兩個﹞<br />
-        但 8 不是質數，因為它的因數有 1, 2, 4, 8 ﹝共四個﹞<br />
+        例如 7 是質數，因為它的因數只有 <span className="frame">1 和 7 </span>﹝共兩個﹞<br />
+        但 8 不是質數，因為它的因數有  <span className="frame"> 1, 2, 4, 8 </span>﹝共四個﹞<br />
         反過來說，8是1, 2, 4, 8各自的倍數 (multiple)。但平常我們很少說8是8的倍數，因為對計算的幫助不太大。
       </p>
       <p>
